d17fa0c1f05a69e4953992546f1ef5634aa73117
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2024-07-01 13:32:01 +0530

    package install

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,25 @@
-const Layout = () => {
-  return <div>Layout</div>;
+//fonts
+import {Sora} from '@next/font/google'
+
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight:['100','200','300','400','500','600','700','800'],
+});
+
+//components
+import Nav from '../components/Nav'
+
+import Header from '../components/Header'
+import TopLeftImg from '../components/TopLeftImg'
+
+const Layout = ({children}) => {
+  return <div>
+    <Nav/>
+    <Header/>
+    <TopLeftImg/>
+    {children}
+  </div>;
 };
 
 export default Layout;
